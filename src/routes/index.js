c400b83b9b1d87033654c7408bbcf11fdbecd08f
Mikee Ramirez <mikee@Mikees-MacBook-Pro.local>
2021-07-13 19:12:46 -0500

    feat: adding validator in routes
    Signed-off-by: Mikee Ramirez <mikee@Mikees-MacBook-Pro.local>

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,10 @@
 const express = require('express');
-const {celebrate, Joi, Segments} = require('celebrate');
 const router = express.Router();
 const UserController = require('../controllers/user');
 const PostController = require('../controllers/posts');
 const verify = require('../middlewares/verify');
-const { UserValidator } = require('../validators');
+const { UserValidator, PostValidator } = require('../validators');
 
-const postCreateValidator = {
-    [Segments.BODY] :{
-        title: Joi.string().required(),
-        body: Joi.string().required(),
-        category: Joi.string().required(),
-        user_id: Joi.number().integer().required(),
-    } //schema de validacion
-}
 
 // user routes
 router.get('/users', verify, UserController.fetch);
@@ -28,7 +19,7 @@ router.get('/users/:id/posts', verify, UserController.populatedUser)
 router.get('/posts/users', verify, PostController.populatedPosts);
 router.get('/posts', verify,PostController.fetch);
 router.get('/posts/:id', verify, PostController.retrieve)
-router.post('/posts', verify, celebrate(postCreateValidator) ,PostController.add)
+router.post('/posts', verify, PostValidator.create ,PostController.add)
 router.put('/posts/:id', verify, PostController.modify)
 router.delete('/posts/:id', verify, PostController.eliminate)
 
