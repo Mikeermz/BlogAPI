877f1490013f44a506b8bccc764a306a6d192655
Mikee Ramirez <mikee@Mikees-MacBook-Pro.local>
2021-07-13 19:56:10 -0500

    feat: separate routes and improts in index
    Signed-off-by: Mikee Ramirez <mikee@Mikees-MacBook-Pro.local>

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,26 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const UserController = require('../controllers/user');
-const PostController = require('../controllers/posts');
-const verify = require('../middlewares/verify');
-const { UserValidator, PostValidator } = require('../validators');
+const AuthMiddleware = require('../middlewares/verify');
+// Rutas indexadas
+const homeRoutes = require('./home');
+const signUpRoutes = require('./signup');
+const userRoutes = require('./user');
 
+// const PostController = require('../controllers/posts');
+// const { PostValidator } = require('../validators');
 
-// user routes
-router.get('/users', verify, UserController.fetch);
-router.get('/users/:id', verify, UserValidator.findOne, UserController.retrieve)
-router.post('/user/login', UserValidator.login ,UserController.login)
-router.post('/users', UserValidator.create, UserController.add)
-router.put('/users/:id', verify, UserValidator.create, UserController.modify)
-router.delete('/users/:id', verify, UserController.eliminate)
-router.get('/users/:id/posts', verify, UserController.populatedUser)
+router.use(homeRoutes);
+router.use('/signup', signUpRoutes);
+router.use('/users', AuthMiddleware, userRoutes);
 
-// posts routes
-router.get('/posts/users', verify, PostController.populatedPosts);
-router.get('/posts', verify,PostController.fetch);
-router.get('/posts/:id', verify, PostController.retrieve)
-router.post('/posts', verify, PostValidator.create ,PostController.add)
-router.put('/posts/:id', verify, PostController.modify)
-router.delete('/posts/:id', verify, PostController.eliminate)
+// // posts routes
+// router.get('/posts/users', verify, PostController.populatedPosts);
+// router.get('/posts', verify,PostController.fetch);
+// router.get('/posts/:id', verify, PostController.retrieve)
+// router.post('/posts', verify, PostValidator.create ,PostController.add)
+// router.put('/posts/:id', verify, PostController.modify)
+// router.delete('/posts/:id', verify, PostController.eliminate)
 
 module.exports =  router;
