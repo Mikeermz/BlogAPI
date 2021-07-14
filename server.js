1fd6e3e4a9491fa11781339bb1172e7fcdb231fd
Mikee Ramirez <mikee@Mikees-MacBook-Pro.local>
2021-07-13 20:39:55 -0500

    feat: adding morgan to logget http request
    Signed-off-by: Mikee Ramirez <mikee@Mikees-MacBook-Pro.local>

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const routes = require('./src/routes');
 const { errors } = require('celebrate')
 const { PORT } = require('./src/constants/config')
@@ -7,6 +8,7 @@ const { PORT } = require('./src/constants/config')
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(morgan('dev'));
 
 app.use('/api/v1', routes);
 app.use(errors())
